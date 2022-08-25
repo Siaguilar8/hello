ce02d069fbcb33cea855ef8d7c647078e6b69212
Samuel Aguilar <saguilar13@huskers.unl.edu>
2022-08-25 20:29:26 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Authors: Samuel Aguilar and Oscar Murcia Guardado
+ *          saguilar13@huskers.unl.edu, omurciaguardado2@huskers.unl.edu
+ * Date: 08/25/2022
+ * Hack 01
+ **/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    printf("Samuel Aguilar\n");
+}
